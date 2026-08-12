4aab4066fd1bd10d0a7ec9bf62090d29e9d03e54
Brahmansh <brahmanshgarg@gmail.com>
2026-08-12 21:04:21 +0530

    inference started but results mixed

--- inference.py
+++ inference.py
@@ -1,6 +1,6 @@
 from speaker_classifier_modules.model import VoiceClassifier
-from speaker_classifier_modules.data import create_dataloaders, split_data
-from speaker_classifier_modules.config import DATA_DIR, BEST_MODEL
+from speaker_classifier_modules.data import transform
+# from speaker_classifier_modules.train import transform
 import torch
 import numpy as np
 import sounddevice as sd
@@ -8,12 +8,14 @@ import scipy.io.wavfile as wav
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import spectrogram
-# test and run ffmpeg to convert m4a to wav
-import subprocess
-import os
-from pathlib import Path
+
 import io
 from PIL import Image
+
+model = VoiceClassifier()
+model.load_state_dict(torch.load(r"D:\audio_classifier\models\best_model_20260812_192422.pth"))
+model.to(device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+
 
 SAMPLE_RATE = 48000  # Define the sample rate for audio processing
 WINDOW_SECONDS = 2
@@ -82,5 +84,23 @@ for audio in record_live():
     # Here you can process the audio window, e.g., convert it to a spectrogram and feed it to the model
     # For demonstration, we will just print the shape of the audio window
     print(f"Received audio window of shape: {audio.shape}")
-    plot_spectrogram(audio, SAMPLE_RATE, "temp_spectrogram.png")
-    break
+    img = plot_spectrogram(audio, SAMPLE_RATE, "temp_spectrogram.png")
+    class_dict = {0: 'ansh', 1: 'varsha'}
+
+    model.eval()
+    transformed_image = transform(img).unsqueeze(0)  # add batch dimension
+
+    # step 4 is to pass the image through the model and get the predicted class
+    with torch.no_grad():
+        transformed_image = transformed_image.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+        output = model(transformed_image)
+        probs = torch.nn.Softmax(dim=1)(output)
+        probs = probs.cpu().numpy()[0]
+        print("Predicted probabilities:", probs)
+        # probabilities and class names
+        for i, prob in enumerate(probs):
+            print(f"Class: {class_dict[i]}, Probability: {prob:.4f}")
+        # _, predicted_class = torch.max(output.data, 1)
+        # print("Predicted class:", predicted_class.item())
+        # print("Predicted class name:", class_dict[predicted_class.item()])
+        
